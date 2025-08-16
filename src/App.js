1076a4fd4f7c64f1f74043c13642bedc2eb0ced8
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:23 +0530

    Initial commit: Blog-Platform

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Blog-Platform"</h1>
+        <p>"Content management system"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
